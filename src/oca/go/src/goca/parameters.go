a2564b78b793c979f57918a54e86f685ce3d33b6
Pierre Lafievre <pierre.lafievre@iguanesolutions.com>
2019-05-21 12:18:33 +0200

    add view notion

--- src/oca/go/src/goca/parameters.go
+++ src/oca/go/src/goca/parameters.go
@@ -16,6 +16,11 @@
 
 package goca
 
+import (
+	"errors"
+	"fmt"
+)
+
 // UpdateType is a parameter to update methods indicating how to replace the template
 type UpdateType int
 
@@ -27,22 +32,111 @@ const (
 	Merge UpdateType = 1
 )
 
-// WhoPool is a parameter to pool info methods allowing to so some filtering
-//type WhoPool int
+// PoolWho is a parameter to pool info methods allowing to so some filtering
+type PoolWho int
 
 const (
 	// PoolWhoPrimaryGroup resources belonging to the user’s primary group.
-	PoolWhoPrimaryGroup = -4
+	PoolWhoPrimaryGroup PoolWho = -4
 
 	// PoolWhoMine to list resources that belong to the user that performs the
 	// query.
-	PoolWhoMine = -3
+	PoolWhoMine PoolWho = -3
 
 	// PoolWhoAll to list all the resources seen by the user that performs the
 	// query.
-	PoolWhoAll = -2
+	PoolWhoAll PoolWho = -2
 
 	// PoolWhoGroup to list all the resources that belong to the group that performs
 	// the query.
-	PoolWhoGroup = -1
+	PoolWhoGroup PoolWho = -1
 )
+
+type View struct {
+	who int
+	id  ViewRangeID
+}
+
+type ViewRangeID struct {
+	start int
+	end   int
+}
+
+// NewViewWho is a helper allowing you to only give the visibility, and optionally the range
+func NewViewWho(who int, args ...int) (View, error) {
+	var f View
+
+	if f.who < int(PoolWhoPrimaryGroup) {
+		return f, fmt.Errorf("bad Who value")
+	}
+
+	f.who = who
+
+	switch len(args) {
+	case 0:
+		f.id = ViewRangeID{-1, -1}
+	case 1:
+		return f, errors.New("Wrong number of arguments: end of range is missing")
+	case 2:
+		f.id = ViewRangeID{args[0], args[1]}
+	default:
+		return f, errors.New("Wrong number of arguments: too much arguments")
+	}
+
+	return f, nil
+}
+
+// NewView is a function returning a View from a list of variable parameters
+func NewView(args ...int) (View, error) {
+	if len(args) > 1 {
+		return NewViewWho(args[0], args...)
+	}
+
+	return View{
+		int(PoolWhoMine),
+		ViewRangeID{-1, -1},
+	}, nil
+
+}
+
+// SetUID enable filtering on a user ID filter.
+// You have to choose between SetUID and SetVisibility methods.
+func (f *View) SetUID(uid int) error {
+	if uid < 0 {
+		return fmt.Errorf("View.SetUID: parameters uid must be positive")
+	}
+	f.who = uid
+	return nil
+}
+
+// SetVisibility enable filtering based on visibility from user/group used to connect to OpenNebula.
+// You have to choose between SetUID and SetVisibility methods.
+func (f *View) SetVisibility(flag PoolWho) error {
+	switch flag {
+	case -4, -3, -2, -1:
+	default:
+		return fmt.Errorf("View.SetVisibility: bad parameter value, should be one of PoolWho values")
+	}
+	f.who = int(flag)
+
+	return nil
+}
+
+func argsToInt(length int, args ...interface{}) ([]int, error) {
+
+	min := length
+	if len(args) < length {
+		min = len(args)
+	}
+
+	argsI := make([]int, 0, min)
+	for i := 0; i < min; i++ {
+		argI, ok := args[i].(int)
+		if !ok {
+			return argsI, fmt.Errorf("wrong type, needs an int")
+		}
+		argsI = append(argsI, argI)
+	}
+
+	return argsI, nil
+}
